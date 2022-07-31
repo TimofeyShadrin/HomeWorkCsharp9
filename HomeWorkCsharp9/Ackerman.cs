9749fdcac50e2dc527de2df9a92e38da5049c634
Timofei Shadrin <tshadrin@yandex.ru>
2022-07-31 22:02:26 +0300

    protected used

--- HomeWorkCsharp9/Ackerman.cs
+++ HomeWorkCsharp9/Ackerman.cs
@@ -5,12 +5,13 @@
         private static uint firstValue = 0;
         private static uint secondValue = 0;
 
-        public static uint FirstValue
+        protected static uint FirstValue
         {
             get { return firstValue; }
             set
             {
-                if (value < 4) firstValue = value;
+                if (value < 4)
+                    firstValue = value;
                 else
                 {
                     firstValue = 3;
@@ -19,24 +20,29 @@
             }
         }
 
-        public static uint SecondValue
+        protected static uint SecondValue
         {
             get { return secondValue; }
             set
             {
-                if (value < 10) secondValue = value;
+                if (value < 10)
+                    secondValue = value;
                 else
                 {
                     secondValue = 10;
-                    Console.Write($"\n\tЗа значение второй переменной принято число: {secondValue}\n");
+                    Console.Write(
+                        $"\n\tЗа значение второй переменной принято число: {secondValue}\n"
+                    );
                 }
             }
         }
 
         public static void FillDataForAckerman()
         {
-            Console.Write("\n\tПожалуйста введите значение первой переменной\n" +
-                "\tв виде натурального числа: ");
+            Console.Write(
+                "\n\tПожалуйста введите значение первой переменной\n"
+                    + "\tв виде натурального числа: "
+            );
             string? startDigit = String.Empty;
             startDigit = Console.ReadLine();
             Console.Write(
@@ -45,8 +51,7 @@
             );
             string? endDigit = String.Empty;
             endDigit = Console.ReadLine();
-            if (!String.IsNullOrEmpty(startDigit)
-                && !String.IsNullOrEmpty(endDigit))
+            if (!String.IsNullOrEmpty(startDigit) && !String.IsNullOrEmpty(endDigit))
             {
                 uint startValue = Convert.ToUInt16(startDigit.ToString());
                 uint endValue = Convert.ToUInt16(endDigit.ToString());
@@ -57,17 +62,19 @@
             {
                 FirstValue = 0;
                 SecondValue = 0;
-                Console.Write("\n\tВаш ввод не соответствует требованиям!\n" +
-                        $"\tЗа значение первой переменной принято число: {FirstValue}");
+                Console.Write(
+                    "\n\tВаш ввод не соответствует требованиям!\n"
+                        + $"\tЗа значение первой переменной принято число: {FirstValue}"
+                );
                 Console.Write($"\n\tЗа значение второй переменной принято число: {SecondValue}");
             }
         }
+
         public static uint AckermanFunction(uint n, uint m)
         {
             if (n == 0)
                 return m + 1;
-            else
-              if ((n != 0) && (m == 0))
+            else if ((n != 0) && (m == 0))
                 return AckermanFunction(n - 1, 1);
             else
                 return AckermanFunction(n - 1, AckermanFunction(n, m - 1));
